63022b013343abf27c14775ba57a7c3854f1f1cf
Augusto Maldonado <augusto.maldonado@openk.com.br>
2025-02-27 18:30:14 -0300

    feat: adicionando api

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("1958563a4afd6012")
+/******/ 		__webpack_require__.h = () => ("3074c5bb81e8dfaa")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "df015d1d0f5b42c5"; }
+/******/ 		__webpack_require__.h = function() { return "f54353989a6734ff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
